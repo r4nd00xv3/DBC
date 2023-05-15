d041dcc4bc27259e442c22fa06389c7e4ca9537f
ramon.deandrade <ramon.deandrade@anvisa.gov.br>
2023-05-15 17:13:05 -0300

    swagger doc

--- src/main/java/com/example/dbc/DbcApplication.java
+++ src/main/java/com/example/dbc/DbcApplication.java
@@ -3,9 +3,13 @@ package com.example.dbc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableTransactionManagement
+@EnableWebMvc
+@EnableSwagger2
 public class DbcApplication {
 
 	public static void main(String[] args) {
